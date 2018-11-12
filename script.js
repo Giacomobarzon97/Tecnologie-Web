70cfbddb0dc21c214a070bc3981db7122eaf66a5
Francesco <frdefilippis@gmail.com>
2018-11-12 19:34:55 +0100

    Rollback

---
+++ script.js
@@ -0,0 +1,8 @@
+function menuCollapse() {
+    var x = document.getElementById('nav-bar-menu');
+    if (x.style.display === "inline") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "inline";
+    }
+  }
