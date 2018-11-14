3c677b25df2d5df7a3e5c92efc6670e577ed66a9
ScrappyCocco <mikelerogeratro@yahoo.it>
2018-11-14 11:30:24 +0100

    Added reply button Script And Style

--- article1/addComment.js
+++ article1/addComment.js
@@ -1,25 +1,42 @@
-window.addEventListener("load", function ( ){
+window.addEventListener("load", function () {
     //Comment Reply
-	var commentReply= document.getElementsByClassName("comment-reply");
-	var previousReplyClick=null;
-	var commentFormDiv= document.createElement("div");
-	commentFormDiv.className="reply-form";
-	var commentForm=document.createElement("form");
+	var commentReply = document.getElementsByClassName("comment-reply-start-button");
+	var previousReplyClick = null;
+	var commentFormDiv = document.createElement("div");
+	commentFormDiv.className = "input-comment";
+
+	var commentForm = document.createElement("form");
 	commentForm.method="POST";
 	commentForm.action="";
-	commentFormDiv.appendChild(commentForm);
+
+	var inputCommentArea = document.createElement("div");
+	inputCommentArea.className="input-comment-area";
+
 	var commentFormTextArea=document.createElement("textarea");
-	commentFormTextArea.rows="7";
-	commentFormTextArea.cols="40";
-	commentForm.appendChild(commentFormTextArea);
-	var commentFormSubmit=document.createElement("input");
-	commentFormSubmit.type="submit";
-	commentForm.appendChild(commentFormSubmit)
+	commentFormTextArea.rows = "7";
+	commentFormTextArea.cols = "100";
+	commentFormTextArea.value = "";
+	commentFormTextArea.placeholder = "Scrivi una risposta..."
+	inputCommentArea.appendChild(commentFormTextArea);
+
+	var commentFormSubmit = document.createElement("input");
+	commentFormSubmit.type = "submit";
+	commentFormSubmit.value = "Invia risposta";
+	var inputCommentFooter = document.createElement("div");
+	inputCommentFooter.className="input-reply-footer";
+	inputCommentFooter.appendChild(commentFormSubmit)
+
+	commentForm.appendChild(inputCommentArea);
+	commentForm.appendChild(inputCommentFooter);
+	commentFormDiv.appendChild(commentForm);
+
 	for(var i=0; i<commentReply.length; i++){
 		commentReply[i].addEventListener("click",function(e){
 			e = e || window.event;
 			var target = e.target || e.srcElement;
 			target.parentNode.appendChild(commentFormDiv);
+			//Azzero la text-area dall'input precedente
+			commentFormDiv.childNodes[0].childNodes[0].childNodes[0].value = "";
 			target.style.display="none";
 			if(previousReplyClick!=null){
 				previousReplyClick.style.display="block"
