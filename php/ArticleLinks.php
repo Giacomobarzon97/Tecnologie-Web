37b7f0dd41c14d50f3ca0706398284b177cb4226
Michele <mikelerogeratro@yahoo.it>
2018-12-17 17:41:47 +0100

    File reformat(?)

--- php/ArticleLinks.php
+++ php/ArticleLinks.php
@@ -10,55 +10,55 @@
 ?>
 <!DOCTYPE html>
 <html lang="it">
-	<head>
-		<title>WebSite-Home</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<meta name="title" content="progetto tecc-web" />
+    <head>
+        <title>WebSite-Home</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="title" content="progetto tecc-web" />
         <meta name="description" content="computer science topics" />
         <meta name="keywords" content="computer science" />
         <meta name="language" content="italian it" />
-		<meta name="author" content="" />
-		<meta content="width=device-width, initial-scale=1" name="viewport" />
-		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"/>		
-		
-		<link rel="stylesheet" type="text/css" href="style.css" />
-		<link rel="stylesheet" type="text/css" href="print.css" media="print"/>
-		<script src="./scripts.js"></script>
-		<?php
+        <meta name="author" content="" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"/>		
+        
+        <link rel="stylesheet" type="text/css" href="style.css" />
+        <link rel="stylesheet" type="text/css" href="print.css" media="print"/>
+        <script src="./scripts.js"></script>
+        <?php
         Sidebar::printSidebarIncludeHeader();
         ?>
-	</head>
+    </head>
     <body>
-    <div id="mobile-sidebar-mask">
-    </div>
-	<div id="sidebar-wrapper">
-    <?php
-        Sidebar::printSidebar();
-    ?>
-    </div>
-    <div id="rightSideWrapper">
-        <?php
-            Sidebar::printNavbar();
-        ?>
-        <div id="main">
-            <div id="content-article-introduction">
-            <?php
-                Subtopics::printTopicIntroduction($_GET["id"]);
-                Subtopics::printInsertSubtopicForm($_SESSION['email']);
-            ?>
-            </div>
-            <div id="content-article-body">
-            <h1>Contenuto del corso</h1>
-                <ul>
-                <?php
-                    Subtopics::printSubtopicsList($_GET["id"]);
-                ?>
-                </ul>
-            </div>
+        <div id="mobile-sidebar-mask">
         </div>
+        <div id="sidebar-wrapper">
         <?php
-		    include_once ('footer.php');
-	    ?>
-    </div>
+            Sidebar::printSidebar();
+        ?>
+        </div>
+        <div id="rightSideWrapper">
+            <?php
+                Sidebar::printNavbar();
+            ?>
+            <div id="main">
+                <div id="content-article-introduction">
+                <?php
+                    Subtopics::printTopicIntroduction($_GET["id"]);
+                    Subtopics::printInsertSubtopicForm($_SESSION['email']);
+                ?>
+                </div>
+                <div id="content-article-body">
+                <h1>Contenuto del corso</h1>
+                    <ul>
+                    <?php
+                        Subtopics::printSubtopicsList($_GET["id"]);
+                    ?>
+                    </ul>
+                </div>
+            </div>
+            <?php
+                include_once ('footer.php');
+            ?>
+        </div>
     </body>
 </html>
