786477141c7dc05994a9434a7a6ba93e794c1c65
Francesco <frdefilippis@gmail.com>
2018-12-18 22:40:06 +0100

    Moved style and scripts files

--- scripts.js
+++ scripts.js
@@ -1,16 +1,33 @@
-window.addEventListener("load", function(){
-	document.getElementById("nav-hamburger").addEventListener("click", function(){
-			document.getElementById("sidebar-wrapper").style.width="70%";
-			document.getElementById("mobile-sidebar-mask").style.display="block";
-			document.getElementById("mobile-sidebar-mask").style.backgroundColor="rgba(0,0,0,0.5)";
-	}, false);
-	document.getElementById("mobile-sidebar-mask").addEventListener("click", function(){
-		document.getElementById("sidebar-wrapper").style.width="0%";
-		document.getElementById("mobile-sidebar-mask").style.backgroundColor="rgba(0,0,0,0.0)";		
-		document.getElementById("mobile-sidebar-mask").style.display="none";
-	}, false);
-	
-	
+
+function openMobileSidebar(){
+	var wrapper=document.getElementById("sidebar-wrapper");
+	var mask=document.getElementById("mobile-sidebar-mask");
+	if(wrapper!=null && mask!=null){
+		wrapper.style.width="70%";
+		mask.style.display="block";
+		mask.style.backgroundColor="rgba(0,0,0,0.5)";
+	}
+}
+function closeMobileSidebar(){
+	var wrapper=document.getElementById("sidebar-wrapper");
+	var mask=document.getElementById("mobile-sidebar-mask");
+	if(wrapper!=null && mask!=null){
+		wrapper.style.width="0%";
+		mask.style.backgroundColor="rgba(0,0,0,0.0)";		
+		mask.style.display="none";
+	}
+}
+function toggleMobileNavMenu(){
+	var menu=document.getElementById("menu");
+	if(menu!=null){
+		if(menu.style.display=="block"){
+			menu.style.display="none";		
+		}else{
+			menu.style.display="block"	;	
+		}
+	}
+}
+function replyMessageBox(){
 	//Comment Reply, create comment div and form
 	var commentReply = document.getElementsByClassName("comment-reply-start-button");
 	var previousReplyClick = null;
@@ -56,35 +73,70 @@ window.addEventListener("load", function(){
 			previousReplyClick=target;
 		},false);
 	}
+}
+function sidebarExpandButtons(){
+	var buttons = document.getElementsByClassName("expand-button");
+	for(var i=0; i<buttons.length;i++){
+		buttons[i].addEventListener("click",function(e){
+			e = e || window.event;
+			var target = e.target || e.srcElement;
+			var sublist= target.parentNode.parentNode.getElementsByTagName("UL")[0];
+			if(sublist.style.display=="block"){
+				target.src="img/expand-button.svg";
+				sublist.style.display="none";
+			}else{
+				sublist.style.display="block";
+				target.src="img/collapse-button.svg";				
+			}
+		},false);
+	}
+}
+window.addEventListener("load", function(){
+	var hamburger=document.getElementById("nav-hamburger");
+	var mask=document.getElementById("mobile-sidebar-mask");
+	var menuIcon=document.getElementById("nav-menu-icon");
+	if(hamburger!=null && mask!=null){
+		hamburger.addEventListener("click",openMobileSidebar, true);
+		mask.addEventListener("click",closeMobileSidebar, true);
+	}
+	if(menuIcon!=null){
+		menuIcon.addEventListener("click",toggleMobileNavMenu , true);
+	}
+	sidebarExpandButtons();
+	replyMessageBox();
 });
 
 window.addEventListener('resize', function(e) {
+	var wrapper=document.getElementById("sidebar-wrapper");
+	var mask=document.getElementById("mobile-sidebar-mask");
+	var menu=document.getElementById("menu");
 	if(window.innerWidth>768){
-		document.getElementById("sidebar-wrapper").style.width="25%";
-		document.getElementById("mobile-sidebar-mask").style.backgroundColor="rgba(0,0,0,0.0)";		
-		document.getElementById("mobile-sidebar-mask").style.display="none";
+		if(wrapper!=null && mask!=null){
+			wrapper.style.width="25%";
+			mask.style.backgroundColor="rgba(0,0,0,0.0)";		
+			mask.style.display="none";
+		}
+		if(menu!=null){
+			menu.style.display="block";
+		}
 	}else{
-		document.getElementById("sidebar-wrapper").style.width="0%";
+		if(wrapper!=null){
+			wrapper.style.width="0%";
+		}
+		if(menu!=null){
+			menu.style.display="none";
+		}
+
 	}
 }, false);
 
 /* nav-bar menu collapse */
 
-function menuCollapse() {
-    var y = document.getElementById('nav-bar-menu');
-    if (y.className === "menu") {
-    	y.className += " responsive";
-    } else {
-    	y.className = "menu";
-	}
-}
-
 document.addEventListener("DOMContentLoaded", function() {
 	//The first argument are the elements to which the plugin shall be initialized
 	//The second argument has to be at least a empty object or a object with your desired options
-	OverlayScrollbars(document.getElementById("sidebar-wrapper"), { });
+	var sidebar=document.getElementById("sidebar-wrapper");
+	if(sidebar!=null){
+		OverlayScrollbars(sidebar, { });
+	}
 });
-
-function jumpto(anchor){
-    window.location.href = "#"+anchor;
-}
