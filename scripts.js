58d22f93994fc3518ef85edd6c194d8e8229fa5b
ROVERATO Michele <mroverat@t21.LABTA>
2018-12-19 13:21:39 +0100

    Pulito script.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-
 function openMobileSidebar(){
 	var wrapper=document.getElementById("sidebar-wrapper");
 	var mask=document.getElementById("mobile-sidebar-mask");
@@ -8,6 +7,7 @@ function openMobileSidebar(){
 		mask.style.backgroundColor="rgba(0,0,0,0.5)";
 	}
 }
+
 function closeMobileSidebar(){
 	var wrapper=document.getElementById("sidebar-wrapper");
 	var mask=document.getElementById("mobile-sidebar-mask");
@@ -17,6 +17,7 @@ function closeMobileSidebar(){
 		mask.style.display="none";
 	}
 }
+
 function toggleMobileNavMenu(){
 	var menu=document.getElementById("menu");
 	if(menu!=null){
@@ -27,53 +28,7 @@ function toggleMobileNavMenu(){
 		}
 	}
 }
-function replyMessageBox(){
-	//Comment Reply, create comment div and form
-	var commentReply = document.getElementsByClassName("comment-reply-start-button");
-	var previousReplyClick = null;
-	var commentFormDiv = document.createElement("div");
-	commentFormDiv.className = "input-comment";
 
-	var commentForm = document.createElement("form");
-	commentForm.method="POST";
-	commentForm.action=""; //To add later, as input names
-
-	var inputCommentArea = document.createElement("div");
-	inputCommentArea.className="input-comment-area";
-
-	var commentFormTextArea=document.createElement("textarea");
-	commentFormTextArea.rows = "7";
-	commentFormTextArea.cols = "100";
-	commentFormTextArea.value = "";
-	inputCommentArea.appendChild(commentFormTextArea);
-
-	var commentFormSubmit = document.createElement("input");
-	commentFormSubmit.type = "submit";
-	commentFormSubmit.value = "Invia risposta";
-	var inputCommentFooter = document.createElement("div");
-	inputCommentFooter.className="input-reply-footer";
-	inputCommentFooter.appendChild(commentFormSubmit)
-
-	commentForm.appendChild(inputCommentArea);
-	commentForm.appendChild(inputCommentFooter);
-	commentFormDiv.appendChild(commentForm);
-
-	//Onclick() status, executed when clicking "Reply" button
-	for(var i=0; i<commentReply.length; i++){
-		commentReply[i].addEventListener("click",function(e){
-			e = e || window.event;
-			var target = e.target || e.srcElement;
-			target.parentNode.appendChild(commentFormDiv);
-			//Azzero la text-area dall'input precedente
-			commentFormDiv.childNodes[0].childNodes[0].childNodes[0].value = "";
-			target.style.display="none";
-			if(previousReplyClick!=null){
-				previousReplyClick.style.display="block";
-			}
-			previousReplyClick=target;
-		},false);
-	}
-}
 function sidebarExpandButtons(){
 	var buttons = document.getElementsByClassName("expand-button");
 	for(var i=0; i<buttons.length;i++){
@@ -91,6 +46,7 @@ function sidebarExpandButtons(){
 		},false);
 	}
 }
+
 window.addEventListener("load", function(){
 	var hamburger=document.getElementById("nav-hamburger");
 	var mask=document.getElementById("mobile-sidebar-mask");
@@ -131,7 +87,6 @@ window.addEventListener('resize', function(e) {
 }, false);
 
 /* nav-bar menu collapse */
-
 document.addEventListener("DOMContentLoaded", function() {
 	//The first argument are the elements to which the plugin shall be initialized
 	//The second argument has to be at least a empty object or a object with your desired options
