830e01fe69bfa464684f3595942cae22e2016604
Michele <mikelerogeratro@yahoo.it>
2018-12-23 13:08:37 +0100

    Indent fixes

--- php/forgotPassword.php
+++ php/forgotPassword.php
@@ -8,29 +8,29 @@
 ?> 
 <!DOCTYPE html>
 <html lang="it">
-	<head>
-		<title>WebSite-Profile</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<meta name="title" content="progetto tec-web" />
+    <head>
+        <title>WebSite-Profile</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="title" content="progetto tec-web" />
         <meta name="description" content="" />
         <meta name="keywords" content="" />
         <meta name="language" content="italian it" />
-		<meta name="author" content="" />
-		<meta content="width=device-width, initial-scale=1" name="viewport" />
-		
+        <meta name="author" content="" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        
         <link rel="stylesheet" type="text/css" href="https://frncscdf.github.io/Tecnologie-Web/style.css" />
-		<link rel="stylesheet" type="text/css" href="print.css" media="print"/>
-		<script src="https://frncscdf.github.io/Tecnologie-Web/scripts.js"></script>
+        <link rel="stylesheet" type="text/css" href="print.css" media="print"/>
+        <script src="https://frncscdf.github.io/Tecnologie-Web/scripts.js"></script>
         
     </head>
     
     <body>
-		<div id="registration-form">
-			<div class="regform-introduction">
-				<h1><a href="index.html">Nome del sito</a></h1>
-				<h2>Hai dimenticato la password? Recuperala!</h2>
-			</div>
-			<div class="regform-main-section">
+        <div id="registration-form">
+            <div class="regform-introduction">
+                <h1><a href="index.html">Nome del sito</a></h1>
+                <h2>Hai dimenticato la password? Recuperala!</h2>
+            </div>
+            <div class="regform-main-section">
             <?php 
                     include_once ('User.php');
 
@@ -44,21 +44,21 @@
                         }
                     } 
                 ?>
-				<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-					<label for="lemail">Email</label>
-					<input class="profile-input" type="text" id="lemail" name="email" placeholder="Email@some.boh" />
-				
-					<input class="profile-input" name="submit" type="submit" value="Recupera" />
-				</form>
-			</div>
-			<div class="regform-side-section">
-				<p>Non sei ancora registrato?
-				<p>Clicca <a href='registrazione.php'>qui</a> per creare un nuovo account.</p>
-			</div>
-			<ul>
-				<li><a href="index.php">Home</a></li>
-				<li><a href="index.php">About</a></li>
-			</ul>
-		</div>	
-	</body>
+                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+                    <label for="lemail">Email</label>
+                    <input class="profile-input" type="text" id="lemail" name="email" placeholder="Email@some.boh" />
+                
+                    <input class="profile-input" name="submit" type="submit" value="Recupera" />
+                </form>
+            </div>
+            <div class="regform-side-section">
+                <p>Non sei ancora registrato?
+                <p>Clicca <a href='registrazione.php'>qui</a> per creare un nuovo account.</p>
+            </div>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="index.php">About</a></li>
+            </ul>
+        </div>	
+    </body>
 </html>
--- php/recoverPassword.php
+++ php/recoverPassword.php
@@ -12,29 +12,29 @@
 ?> 
 <!DOCTYPE html>
 <html lang="it">
-	<head>
-		<title>WebSite-Profile</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<meta name="title" content="progetto tec-web" />
+    <head>
+        <title>WebSite-Profile</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="title" content="progetto tec-web" />
         <meta name="description" content="" />
         <meta name="keywords" content="" />
         <meta name="language" content="italian it" />
-		<meta name="author" content="" />
-		<meta content="width=device-width, initial-scale=1" name="viewport" />
-		
+        <meta name="author" content="" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        
         <link rel="stylesheet" type="text/css" href="https://frncscdf.github.io/Tecnologie-Web/style.css" />
-		<link rel="stylesheet" type="text/css" href="print.css" media="print"/>
-		<script src="https://frncscdf.github.io/Tecnologie-Web/scripts.js"></script>
+        <link rel="stylesheet" type="text/css" href="print.css" media="print"/>
+        <script src="https://frncscdf.github.io/Tecnologie-Web/scripts.js"></script>
         
     </head>
     
     <body>
-		<div id="registration-form">
-			<div class="regform-introduction">
-				<h1><a href="index.html">Nome del sito</a></h1>
-				<h2>Reimposta la password</h2>
-			</div>
-			<div class="regform-main-section">
+        <div id="registration-form">
+            <div class="regform-introduction">
+                <h1><a href="index.html">Nome del sito</a></h1>
+                <h2>Reimposta la password</h2>
+            </div>
+            <div class="regform-main-section">
             <?php 
                     include_once ('User.php');
 
@@ -47,13 +47,13 @@
                         }
                     } 
                 ?>
-				<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
                 <label for="lpassword">Password</label>
-				<input class="profile-input" type="text" id="lpassword" name="password" placeholder="Password" />
+                <input class="profile-input" type="text" id="lpassword" name="password" placeholder="Password" />
 
                 <label for="lpassword-confirm">Conferma la Password</label>
-				<input class="profile-input" type="text" id="lpassword-confirm" name="password" placeholder="Conferma la Password" />
-				
+                <input class="profile-input" type="text" id="lpassword-confirm" name="password" placeholder="Conferma la Password" />
+                
                 <?php
                 if(isset($_GET['token'])){ //Alla prima apertura stampo il token dal get
                     echo '<input type="hidden" name="change_pw_token" value="'.$_GET['token'].'" />';
@@ -61,17 +61,17 @@
                     echo '<input type="hidden" name="change_pw_token" value="'.$_POST['change_pw_token'].'" />';
                 }
                 ?>
-				<input class="profile-input" name="submit" type="submit" value="Reimposta" />
-				</form>
-			</div>
-			<div class="regform-side-section">
-				<p>Non sei ancora registrato?
-				<p>Clicca <a href='registrazione.php'>qui</a> per creare un nuovo account.</p>
-			</div>
-			<ul>
-				<li><a href="index.php">Home</a></li>
-				<li><a href="index.php">About</a></li>
-			</ul>
-		</div>	
-	</body>
+                <input class="profile-input" name="submit" type="submit" value="Reimposta" />
+                </form>
+            </div>
+            <div class="regform-side-section">
+                <p>Non sei ancora registrato?
+                <p>Clicca <a href='registrazione.php'>qui</a> per creare un nuovo account.</p>
+            </div>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="index.php">About</a></li>
+            </ul>
+        </div>	
+    </body>
 </html>
