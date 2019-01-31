625c2c0e5cf390bdf8b7de10f07311c8b4cd5304
Michele <mikelerogeratro@yahoo.it>
2019-01-31 14:26:49 +0100

    Adjusted error page with a better image

--- php/errore.php
+++ php/errore.php
@@ -1,37 +1,37 @@
 <?php
-	session_start();
-	include_once('Card.php');
+    session_start();
+    include_once('Card.php');
 ?>
 <!DOCTYPE html>
 <html lang="it">
-	<head>
-	<title>Errore &#124; DevSpace</title>
-		<meta charset="UTF-8">
+    <head>
+    <title>Errore &#124; DevSpace</title>
+        <meta charset="UTF-8">
         <meta name="description" content="Pagina gestione errori" />
         <meta name="keywords" content="computer, science, informatica, development, teconologia, technology" />
         <meta name="language" content="italian it" />
-		<meta name="author" content="Barzon Giacomo, De Filippis Francesco, Greggio Giacomo, Roverato Michele" />
-		<meta name="theme-color" content="#F5F5F5" />
-		<meta content="width=device-width, initial-scale=1" name="viewport" />
-		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"/>	
-		
-		<link rel="stylesheet" type="text/css" href="https://frncscdf.github.io/Tecnologie-Web/style.css" />
-		<link rel="stylesheet" type="text/css" href="https://frncscdf.github.io/Tecnologie-Web/print.css" media="print"/>
-		<script src="https://frncscdf.github.io/Tecnologie-Web/scripts.js"></script>
-		
-	</head>
-	
-	<body>	
+        <meta name="author" content="Barzon Giacomo, De Filippis Francesco, Greggio Giacomo, Roverato Michele" />
+        <meta name="theme-color" content="#F5F5F5" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"/>	
+        
+        <link rel="stylesheet" type="text/css" href="https://frncscdf.github.io/Tecnologie-Web/style.css" />
+        <link rel="stylesheet" type="text/css" href="https://frncscdf.github.io/Tecnologie-Web/print.css" media="print"/>
+        <script src="https://frncscdf.github.io/Tecnologie-Web/scripts.js"></script>
+        
+    </head>
+    
+    <body>	
         <?php
-			
             include_once ('Errori.php');
+            
             if(isset($_GET['errorCode']) && $_GET['errorCode'] != "") {
                 echo $errors[$_GET['errorCode']];
             } else{
                 echo $errors['404'];
-			}
-			echo "<img src='img/404.jpg' alt='Immagine errore' />";
-		?>
-		
-	</body>
+            }
+            echo "<img src='img/404.png' alt='Immagine errore 404' id='error_404_image' />";
+        ?>
+        
+    </body>
 </html>
