a070615fc132e2e0889d9974d71030b05e6a64d7
Giacomo <43755172+giacomogreggio@users.noreply.github.com>
2019-02-10 18:39:22 +0100

    ultimo tentativo giuro

--- scripts.js
+++ scripts.js
@@ -781,11 +781,20 @@ document.addEventListener("DOMContentLoaded", function () {
 //Gestione scroll verso l'alto
 //----------------------------------------------------
 
-addEventListener(document, "touchstart", function(e) {
-    console.log(e.defaultPrevented);  // will be false
-    e.preventDefault();   // does nothing since the listener is passive
-    console.log(e.defaultPrevented);  // still false
-  }, Modernizr.passiveeventlisteners ? {passive: true} : false);
+// Test via a getter in the options object to see if the passive property is accessed
+var supportsPassive = false;
+try {
+  var opts = Object.defineProperty({}, 'passive', {
+    get: function() {
+      supportsPassive = true;
+    }
+  });
+  window.addEventListener("testPassive", null, opts);
+  window.removeEventListener("testPassive", null, opts);
+} catch (e) {}
+
+// Use our detect's results. passive applied if supported, capture will be false either way.
+document.addEventListener('touchstart',supportsPassive ? { passive: true } : false); 
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.addEventListener("scroll", function () {
